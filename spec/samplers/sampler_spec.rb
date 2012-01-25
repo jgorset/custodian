228c909e595844018491acac478c1b99560004e1
Johannes Gorset <jgorset@gmail.com>
2012-01-25 23:30:25 +0100

    Test sampler description

--- spec/samplers/sampler_spec.rb
+++ spec/samplers/sampler_spec.rb
@@ -4,11 +4,15 @@ require "samplers/helpers"
 describe Custodian::Samplers::Sampler do
 
   before do
-    @sampler = Custodian::Samplers::Sampler.new
+    @sampler = Custodian::Samplers::Sampler
   end
 
   it "can't sample" do
-    expect { @sampler.sample }.to raise_error NotImplementedError
+    expect { @sampler.new.sample }.to raise_error NotImplementedError
+  end
+  
+  it "can be described" do
+    @sampler.description.should equal nil
   end
 
 end
