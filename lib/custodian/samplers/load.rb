a38b5c8b73dfcfd320699a1649148224a2af8a8b
Johannes Gorset <jgorset@gmail.com>
2012-01-25 18:02:26 +0100

    Prune unnecessary comments

--- lib/custodian/samplers/load.rb
+++ lib/custodian/samplers/load.rb
@@ -1,14 +1,6 @@
 module Custodian
   module Samplers
 
-    # = Who
-    #
-    # Sample the load averages.
-    #
-    # == Compatibility
-    #
-    # This sampler should be compatible with any system that has
-    # the <tt>uptime</tt> command.
     class Load < Custodian::Samplers::Sampler
       describe "Load averages"
 
@@ -16,11 +8,9 @@ module Custodian
         command_exists? "uptime"
       end
 
-      # Return a list of strings describing logged in users.
       def sample
         `uptime`[/load averages: (.*)/, 1].split
       end
-
     end
 
   end
--- lib/custodian/samplers/who.rb
+++ lib/custodian/samplers/who.rb
@@ -1,14 +1,6 @@
 module Custodian
   module Samplers
 
-    # = Who
-    #
-    # Sample the logged in users on the system.
-    #
-    # == Compatibility
-    #
-    # This sampler should be compatible with any system that has
-    # the <tt>who</tt> command.
     class Who < Custodian::Samplers::Sampler
       describe "Logged in users"
 
@@ -16,11 +8,9 @@ module Custodian
         command_exists? "who"
       end
 
-      # Return a list of strings describing logged in users.
       def sample
         `who`.lines.collect { |line| line.split.first }.uniq
       end
-
     end
 
   end
