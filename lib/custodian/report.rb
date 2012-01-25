3415567c367a1ad5f0120d08b8bbf772c8a112a3
Johannes Gorset <jgorset@gmail.com>
2012-01-25 17:28:48 +0100

    Include sampler description in report

--- lib/custodian/report.rb
+++ lib/custodian/report.rb
@@ -9,20 +9,12 @@ module Custodian
       @sampler = sampler
     end
 
-    # Return the human-readable name of the sampler this
-    # report was generated from.
-    def name
-      @sampler.class.name.split("::").last.downcase
-    end
-
-    # Return the value of this report.
-    def value
-      @sampler.sample
-    end
-
     def to_hash
       {
-        name => value
+        @sampler.class.name.demodulize.underscore => {
+          :description => @sampler.class.description,
+          :sample => @sampler.sample
+        }
       }
     end
 
