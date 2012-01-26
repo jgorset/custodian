16581c481bab4b816423aac852b9ad01cf3bef89
Johannes Gorset <jgorset@gmail.com>
2012-01-26 23:57:00 +0100

    Revise load average sampler

--- lib/custodian/samplers/darwin/load.rb
+++ lib/custodian/samplers/darwin/load.rb
@@ -5,7 +5,13 @@ module Custodian
       describe "Load averages"
 
       def sample
-        `uptime`[/load averages: (.*)/, 1].split
+        uptime = `uptime`[/load averages: (.*)/, 1].split
+        
+        {
+          "Last 5 minutes"    => uptime[0],
+          "Last 10 minutes"   => uptime[1],
+          "Last 15 minutes"   => uptime[2]
+        }
       end
     end
 
