b0567a2c7812e2ebc5f492e5d98775636b2dd9f1
Johannes Gorset <jgorset@gmail.com>
2012-01-27 00:07:42 +0100

    Add sampler for CPU usage

---
+++ lib/custodian/samplers/darwin/cpu.rb
@@ -0,0 +1,19 @@
+module Custodian
+  module Samplers
+
+    class CPU < Custodian::Samplers::Sampler
+      describe "CPU usage"
+
+      def sample
+        cpu = `top -l 1`.match /CPU usage: ([0-9]+\.[0-9]+%) user, ([0-9]+\.[0-9]+%) sys, ([0-9]+\.[0-9]+%) idle/
+        
+        {
+          "User"    => cpu[1],
+          "System"  => cpu[2],
+          "Idle"    => cpu[3]
+        }
+      end
+    end
+
+  end
+end
