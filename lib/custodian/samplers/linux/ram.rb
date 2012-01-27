2af91c250f2f974ae0be2352c0e2ac831d7ff5e6
Johannes Gorset <jgorset@gmail.com>
2012-01-27 23:55:35 +0100

    Add RAM sampler for Linux

---
+++ lib/custodian/samplers/linux/ram.rb
@@ -0,0 +1,21 @@
+module Custodian
+  module Samplers
+
+    class RAM < Custodian::Samplers::Sampler
+      describe "RAM usage"
+
+      def sample
+        free = `free`.match /Mem: +([0-9]+) +([0-9]+) +([0-9]+) +([0-9]+) +([0-9]+) +([0-9]+)/
+
+        {
+          "Total"   => "#{free[1]} MB",
+          "Used"    => "#{free[2]} MB",
+          "Free"    => "#{free[3]} MB",
+          "Buffers" => "#{free[4]} MB",
+          "Cached"  => "#{free[5]} MB"
+        }        
+      end
+    end
+
+  end
+end
