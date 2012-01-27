f64a8d24b589cd39fdb24f3a48d3a99d3725ccf4
Johannes Gorset <jgorset@gmail.com>
2012-01-28 00:01:49 +0100

    Add load sampler for Linux

---
+++ lib/custodian/samplers/linux/load.rb
@@ -0,0 +1,19 @@
+module Custodian
+  module Samplers
+
+    class Load < Custodian::Samplers::Sampler
+      describe "Load averages"
+
+      def sample
+        uptime = `uptime`.match /load average: ([0-9.]+), ([0-9.]+), ([0-9.]+)/
+
+        {
+          "Last 5 minutes"    => uptime[1],
+          "Last 10 minutes"   => uptime[2],
+          "Last 15 minutes"   => uptime[3]
+        }
+      end
+    end
+
+  end
+end
